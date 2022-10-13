0b7d7154ef1a7c4bfc4d2d1cc06ed5c36ddf3cb0
tashisrael <tashisrael04@gmail.com>
2022-10-13 15:04:52 +0300

    Fix some errors

--- src/Modules/addScores.js
+++ src/Modules/addScores.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/m0tkZl4d7IVkemOYTCg2fUdz/scores/';
+const API_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/x0tkZl4d7IVkemOYTCg2fUdz/scores/';
 
 // add score to the API.
 const AddScore = async (user, message) => {
--- src/Modules/fetchScores.js
+++ src/Modules/fetchScores.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/m0tkZl4d7IVkemOYTCg2fUdz/scores/';
+const API_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/x0tkZl4d7IVkemOYTCg2fUdz/scores/';
 
 // get data from the API
 const getScore = async () => {
